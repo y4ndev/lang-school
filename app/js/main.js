aaa234e832e92bcd76ca67c2fbe065669abe4776
Yan Golovchenko <y4nweb@gmail.com>
2022-11-26 13:36:14 +0300

    v.1.0

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+new Swiper(".courses__slides", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  direction: "horizontal",
+  loop: true,
+  slidesPerGroup: 3,
+  slidesPerView: 3,
+  spaceBetween: 40,
+});
+
+
